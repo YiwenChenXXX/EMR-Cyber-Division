035e30c942d3aa6352ed7075a995b19bffe882ff
Yiwen (Fiona) Chen <chenyiwen@ufl.edu>
2026-03-17 17:43:22 -0400

    Implemented Report struct to data_common.h

--- scripts/data_common.h
+++ scripts/data_common.h
@@ -2,3 +2,11 @@
 #include <string>
 #include <vector>
 using namespace std;
+
+struct Report{
+  int report_id;
+  int source_id;
+  long long timestamp;
+  int severity; // from 1 to 10
+  std::string category;  // e.g., medical/fire/flood/rescue/power
+};
