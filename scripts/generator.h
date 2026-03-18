70724f804e1ad5c9ab679689b4f99441cdf2946d
Yiwen (Fiona) Chen <chenyiwen@ufl.edu>
2026-03-18 17:38:14 -0400

    Add function declaration for generateDatasetCSV

--- scripts/generator.h
+++ scripts/generator.h
@@ -1,1 +1,9 @@
+#pragma once
+#include <string>
+using namespace std;
 
+bool generateDatasetCSV(const string& outPath,
+                        int nReports,
+                        int nSources,
+                        unsigned int seed,
+                        long long startTs);
