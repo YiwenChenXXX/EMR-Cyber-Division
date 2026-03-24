1c0d1d305738c2848880958a5433af57679a8c0e
Yiwen (Fiona) Chen <chenyiwen@ufl.edu>
2026-03-24 00:11:36 -0400

    Fix BinomialMaxHeap to compile and run together with other code
    add Node wrapper, complete extractMax, align priority rule

--- scripts/BinomialMaxHeap.h
+++ scripts/BinomialMaxHeap.h
@@ -1,9 +1,5 @@
-
-
 #ifndef COP3530_PROJECT2_BINOMIALMAXHEAP_H
 #define COP3530_PROJECT2_BINOMIALMAXHEAP_H
-
-#endif //COP3530_PROJECT2_BINOMIALMAXHEAP_H
 
 #include <iostream>
 #include <string>
@@ -15,28 +11,36 @@ using namespace std;
 
 class BinomialMaxHeap {
 private:
-    Report* head;
-public:
-    BinomialMaxHeap() {
-        head = nullptr;
+    struct Node {
+        Report key;
+        Node* parent;
+        Node* child;
+        Node* sibling;
+        int degree;
+        explicit Node(const Report& r):key(r), parent(nullptr), child(nullptr), sibling(nullptr), degree(0) {}
     };
+    Node* head;
+    static bool higherPriority(const Report& a, const Report& b);
+
+public:
+    BinomialMaxHeap(){ 
+        head = nullptr; 
+    }
+    bool isEmpty() const{ 
+        return head == nullptr;
+    }
     void insert(int severity);
-    Report* mergeHeaps(Report* h1, Report* h2);
+    void insert(const Report& report);
+    Node* mergeHeaps(Node* h1, Node* h2);
     void unionHeaps();
-    Report* extractMax();
-    Report* findMax();
-
-
-    void linkTrees(Report* a, Report* b) {
-        if (a->severity > b->severity)
-        {
-            std::swap(a, b);
-        }
-
-
+    Report extractMax();
+    Report findMax();
+    void linkTrees(Node* a, Node* b) {
         a->parent = b;
         a->sibling = b->child;
         b->child = a;
         b->degree++;
     }
 };
+
+#endif // COP3530_PROJECT2_BINOMIALMAXHEAP_H
